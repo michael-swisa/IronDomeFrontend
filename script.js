8761c2752884804f7d62a89ccd0f2fde983df5c0
Michael Swisa <ms0504109182@gmail.com>
2024-08-12 15:23:24 +0300

    Addition of deleting missiles as soon as they are sent and when they are intercepted or dropped

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let missile;
+let misseiles;
 
 // Loding missiles.json
 const LodMissilesJson = async () => {
   const response = await fetch("missiles.json");
-  const misseiles = await response.json();
+  const miss = await response.json();
+  misseiles = miss;
+};
 
+// inserting missile into html
+const insertMissileToDispatch = () => {
   for (let i = 0; i < misseiles.length; i++) {
     let missilesDiv = document.getElementById("missiles-to-send");
     let p = document.createElement("p");
     p.innerText = `Missile name: ${misseiles[i]["Name"]}, time: ${misseiles[i]["Time"]}`;
+    p.id = misseiles[i]["Id"];
     missilesDiv.appendChild(p);
   }
-  missile = misseiles;
 };
 
 const setTime = (time) => {
@@ -24,14 +28,17 @@ const setTime = (time) => {
 // Inserting missile into html and send missile to server
 const AddMissiles = async () => {
   await LodMissilesJson();
-  while (missile.length > 0) {
+  insertMissileToDispatch()
+  while (misseiles.length > 0) {
     let missilesDiv = document.getElementById("missiles-in-air");
     let p = document.createElement("p");
-    const OneMissiles = missile.shift();
+    const OneMissiles = misseiles.shift();
     if (OneMissiles) {
       let AmountTime = OneMissiles.Time;
       await new Promise((resolve) => setTimeout(resolve, AmountTime * 1000));
+      removeMissile("missiles-to-send", OneMissiles["Id"]);
       publishMessage(OneMissiles);
+      p.id = OneMissiles["Id"];
       p.innerText += `Missile name: ${OneMissiles["Name"]}.`;
       missilesDiv.appendChild(p);
     }
@@ -90,15 +97,34 @@ const publishMessage = (miss) => {
 socket.onmessage = (event) => {
   console.log("got message", JSON.stringify(event.data));
   let result = JSON.parse(event.data);
+  removeMissile("missiles-in-air", result.Id);
+  printToScreenResult(result);
+};
+
+
+const removeMissile = (nameDiv, id) => {
+  const missileOnAir = document.getElementById(nameDiv).children;
+  for (let i = 0; i < missileOnAir.length; i++) {
+    if (missileOnAir[i].id == id) {
+      missileOnAir[i].remove();
+    }
+  }
+};
+
+
+// Print to the screen the result of the interception
+const printToScreenResult = (result) => {
   if (result.intercepted) {
     const missilesDivs = document.getElementById("intercepted-missiles");
     const p = document.createElement("p");
     p.innerText = `Missile name: ${result.Missile}, intercepted: ${result.intercepted} by: ${result.by}`;
+    p.id = result.Id;
     missilesDivs.appendChild(p);
   } else {
     const missilesDivs = document.getElementById("Fallen-missiles");
     const p = document.createElement("p");
     p.innerText = `Missile name: ${result.Missile}, intercepted: ${result.intercepted} by: ${result.by}`;
+    p.id = result.Id;
     missilesDivs.appendChild(p);
   }
 };
